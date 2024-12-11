7774d979d00a45deee5dfd84758a44fdcc27f6ec
Alejo-P <marcelo.pinzon@epn.edu.ec>
2024-12-10 23:09:05 -0500

    Eliminar línea en el componente PrivateRoute para simplificar la verificación de autenticación

--- Frontend/src/routes/PrivateRoute.jsx
+++ Frontend/src/routes/PrivateRoute.jsx
@@ -3,6 +3,5 @@ import { Navigate } from 'react-router-dom';
 export const PrivateRoute = ({ children }) => 
 {
     const autenticado = localStorage.getItem('token')
-    
     return (autenticado) ? children : <Navigate to='/login' />
 }
